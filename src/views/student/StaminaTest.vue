e6de7b782414dbb53b55249308cabfbcf4c15cc7
huwenqi <171484967@qq.com>
2017-11-18 19:17:08 +0800

    未完成

--- src/views/student/StaminaTest.vue
+++ src/views/student/StaminaTest.vue
@@ -1,9 +1,56 @@
 <template>
     <div class="page-container">
         <div class="main-panel">
-
             <!-- 体测列表 -->
             <div class="table-panel">
+                <el-form :inline="true" :model="filters">
+                    <el-form-item label="姓名">
+                        <el-input v-model="filters.studentName" placeholder="输入学生姓名"></el-input>
+                    </el-form-item>
+                    <el-form-item label="学号">
+                        <el-input v-model="filters.studentNo" placeholder="输入学号"></el-input>
+                    </el-form-item>
+                    <el-form-item label="班级">
+                        <el-input v-model="filters.className" placeholder="输入学生班级"></el-input>
+                    </el-form-item>
+                    <el-form-item label="体测结果">
+                        <el-select class="sm" v-model="filters.result" placeholder="体测结果">
+                            <el-option label="没测" value="1"></el-option>
+                            <el-option label="免测" value="2"></el-option>
+                            <el-option label="不及格" value="3"></el-option>
+                        </el-select>
+                    </el-form-item>
+                    <br>
+                    <el-form-item label="学年">
+                        <el-select class="sm" v-model="filters.schoolYear" placeholder="学年">
+                            <el-option v-for="item in options.schoolYear" :key="item.id" :label="item.schoolYear" :value="item.schoolYear"></el-option>
+                        </el-select>
+                    </el-form-item>
+                    <el-form-item label="学期">
+                        <el-select class="sm" v-model="filters.term" placeholder="学期">
+                            <el-option label="第一学期" value="1"></el-option>
+                            <el-option label="第二学期" value="2"></el-option>
+                        </el-select>
+                    </el-form-item>
+                    <el-form-item label="教师姓名">
+                        <el-select class="sm" v-model="filters.teacherName" placeholder="教师姓名">
+                            <el-option v-for="item in options.teacherName" :key="item.id" :label="item.teacherName" :value="item.teacherName"></el-option>
+                        </el-select>
+                    </el-form-item>
+                    <el-form-item label="课程名">
+                        <el-select class="sm" v-model="filters.courseName" placeholder="课程名">
+                            <el-option v-for="item in options.courseName" :key="item.id" :label="item.courseName" :value="item.courseName"></el-option>
+                        </el-select>
+                    </el-form-item>
+                    <el-form-item label="课程时间">
+                        <el-select class="sm" v-model="filters.courseTime" placeholder="课程时间">
+                            <el-option v-for="item in options.courseTime" :key="item.id" :label="item.courseTime" :value="item.courseTime"></el-option>
+                        </el-select>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-button type="primary" @click="getPhysicalTest">筛选</el-button>
+                    </el-form-item>
+                </el-form>
                 <el-table :data="physicalList">
                     <el-table-column prop="studentNo" label="学号" fixed>
                     </el-table-column>
@@ -54,16 +101,6 @@
                     </el-table-column>
                     <el-table-column prop="sitUps" label="仰卧起坐成绩">
                     </el-table-column>
-                    <el-table-column prop="sitUpsPoint" label="仰卧起坐得分" v-if="sexType === 'girls'">
-                    </el-table-column>
-                    <el-table-column prop="sitUpsLevel" label="仰卧起坐等级" v-if="sexType === 'girls'">
-                    </el-table-column>
-                    <el-table-column prop="pushUps" label="引体向上成绩" v-if="sexType === 'boys'">
-                    </el-table-column>
-                    <el-table-column prop="pushUpsPoint" label="引体向上得分" v-if="sexType === 'boys'">
-                    </el-table-column>
-                    <el-table-column prop="pushUpsLevel" label="引体向上等级" v-if="sexType === 'boys'">
-                    </el-table-column>
                     <el-table-column prop="fiftyRun" label="50米跑成绩">
                     </el-table-column>
                     <el-table-column prop="fiftyRunPoint" label="50米跑得分">
@@ -71,16 +108,6 @@
                     <el-table-column prop="fiftyRunLevel" label="50米跑等级">
                     </el-table-column>
                     <el-table-column prop="eightHundredRun" label="800米跑成绩">
-                    </el-table-column>
-                    <el-table-column prop="eightHundredPoint" label="800米跑得分" v-if="sexType === 'girls'">
-                    </el-table-column>
-                    <el-table-column prop="eightHundredLevel" label="800米跑等级" v-if="sexType === 'girls'">
-                    </el-table-column>
-                    <el-table-column prop="oneThousandRun" label="1000米跑成绩" v-if="sexType === 'boys'">
-                    </el-table-column>
-                    <el-table-column prop="oneThousandPoint" label="1000米跑得分" v-if="sexType === 'boys'">
-                    </el-table-column>
-                    <el-table-column prop="oneThousandLevel" label="1000米跑等级" v-if="sexType === 'boys'">
                     </el-table-column>
                     <el-table-column prop="startTime" label="运动开始时间" width="170">
                     </el-table-column>
@@ -93,8 +120,6 @@
             </div>
             <!-- 体测列表 end -->
         </div>
-
-
     </div>
 </template>
 
@@ -125,78 +150,103 @@
                 }
         }     
     }
-    `
+    `;
     const classPhysicalQuery = `
-    query(
-        $pageNumber: Int
-        $pageSize: Int
-        $classId: Long
-    ){
-        allstudents:searchStudents(
-            classId: $classId
-            pageNumber: $pageNumber
-            pageSize: $pageSize
+        query(
+            $pageNumber: Int
+            $pageSize: Int
+            $classId: Long
         ){
-                pageNum
-                pageSize
-                pagesCount
-                dataCount
-                data {
-                    name
-                    isMan
-                    physicalFitnessTest {
-                        isMan
+            allstudents:searchStudents(
+                classId: $classId
+                pageNumber: $pageNumber
+                pageSize: $pageSize
+            ){
+                    pageNum
+                    pageSize
+                    pagesCount
+                    dataCount
+                    data {
                         name
-                        className
+                        isMan
+                        physicalTest {
+                            isMan
+                            name
+                            className
+                        }
                     }
-                }
+            }
         }
-    }
+    `;
+
+    const conditionQuery = `
+        query($universityId: Long){
+            university(id: $universityId){
+                teacherNameList{
+                    id
+                    teacherName
+                }
+                schoolYearList{
+                    id
+                    schoolYear
+                }
+                courseNameList{
+                    id
+                    courseName
+                }
+                courseTimeList{
+                    id
+                    courseTime
+                }
+            }
+        }
     `
     export default {
         data() {
             return {
                 universityId: resources.universityId,
                 classId: this.$route.params.classId,
-                sexType: 'boys',
                 pageSize: 10,
                 pageNumber: 1,
                 dataCount: 0,
                 loading: false,
+                options: {
+                    teacherName: null,
+                    schoolYear: null,
+                    courseName: null,
+                    courseTime: null,
+                },
                 filters: {
-                    isMan: true,
+                    studentName: '',
+                    studentNo: '',
+                    className: '',
+                    result: '',
+                    schoolYear: '',
+                    term: '',
+                    teacherName: '',
+                    courseName: '',
+                    courseTime: '',
                     pageSize: 0,
                     pageNumber: 0,
                     classId: 0,
                 },
-                physicalList: []
+                physicalList: [],
             }
         },
         //hwq 页面显示的数据
         methods: {
-            changeSex() {
-                let _this = this;
-                _this.pageNumber = 1;
-            },
             getClassPhysicalTest() {
                 let _this = this;
-                if(_this.sexType == "boys"){
-                    _this.filters.isMan = true;
-                }else{
-                    _this.filters.isMan = false;
-                }
                 _this.filters.pageSize = _this.pageSize;
                 _this.filters.pageNumber = _this.pageNumber;
                 _this.filters.classId = _this.classId;
                 _this.physicalList = [];
-                alert(this.physicalList.length)
                 this.$ajax.post(`${resources.graphQlApi}`,{
                                 'query': `${classPhysicalQuery}`,
                                 variables: _this.filters
                     })
                     .then(res => {
                         _this.dataCount = res.data.data.allstudents.dataCount;
-                        //alert(res.data.data.allstudents.data.length)
                         this.physicalList = res.data.data.allstudents.data;
                         console.log(this.physicalList)
                         for (var i = 0;i < _this.physicalList.length; i++){
@@ -205,17 +255,10 @@
                                 i -- ;
                             }
                         }
-                        alert(this.physicalList.length)
-                        console.log(this.physicalList)
                     });
             },
             getAllPhysicalTest() {            
                 let _this = this;
-                if(_this.sexType == "boys"){
-                    _this.filters.isMan = true;
-                }else{
-                    _this.filters.isMan = false;
-                }
                 _this.filters.pageSize = _this.pageSize;
                 _this.filters.pageNumber = _this.pageNumber;
                 _this.physicalList = [];
@@ -236,10 +279,31 @@
                 } else {
                     this.getClassPhysicalTest();
                 }
+            },
+            getSelectCondition(){
+                let _this = this;
+                _this.schoolYearList = [];
+                _this.teacherNameList = [];
+                _this.courseNameList = [];
+                _this.courseTimeList = [];
+                this.$ajax.post(`${resources.graphQlApi}`, {
+                                'query': `${conditionQuery}`,
+                                variables: {
+                                    "universityId":this.universityId
+                                }       
+                    })
+                .then(res => {
+                     _this.options.schoolYear = res.data.data.university.schoolYearList;
+                     _this.options.teacherName = res.data.data.university.teacherNameList;
+                     _this.options.courseName = res.data.data.university.courseNameList;
+                     _this.options.courseTime = res.data.data.university.courseTimeList;
+                });
             }
+
         },
 
         mounted: function () {
+            this.getSelectCondition(),
             this.getPhysicalTest()
         }
     }
