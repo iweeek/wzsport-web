b8ebbbad5f90380b5559262ee0f3678d9130f006
huwenqi <171484967@qq.com>
2017-11-21 16:37:42 +0800

    see f_1133_4

--- src/views/student/StaminaTest.vue
+++ src/views/student/StaminaTest.vue
@@ -25,23 +25,21 @@
                     </el-form-item>
                     <br>
                     <el-form-item label="学年">
-                        <el-select class="sm" v-model="filters.schoolYear" placeholder="学年">
+                        <el-select class="sm" v-model="filters.schoolYear" placeholder="学年" value-key="id" v-on:change="getSelectCondition">
                             <el-option v-for="item in options.schoolYear" :key="item.id" :label="item.schoolYear" :value="item.schoolYear"></el-option>
                         </el-select>
                     </el-form-item>
                     <el-form-item label="学期">
-                        <el-select class="sm" v-model="filters.term" placeholder="学期">
+                        <el-select class="sm" v-model="filters.term" placeholder="学期" v-on:change="getSelectCondition">
                             <el-option label="第一学期" value="1"></el-option>
                             <el-option label="第二学期" value="2"></el-option>
                         </el-select>
                     </el-form-item>
                     <el-form-item label="教师姓名">
-                        <el-select class="sm" v-model="filters.teacherName" placeholder="教师姓名">
-                            <el-option v-for="item in options.teacherName" :key="item.id" :label="item.teacherName" :value="item.teacherName"></el-option>
-                        </el-select>
+                        <el-autocomplete class="inline-input" v-model="filters.teacherName" :fetch-suggestions="querySearch" placeholder="教师姓名" @select="getSelectCondition"></el-autocomplete>
                     </el-form-item>
                     <el-form-item label="课程名">
-                        <el-select class="sm" v-model="filters.courseName" placeholder="课程名">
+                        <el-select class="sm" v-model="filters.courseName" placeholder="课程名" value-key="id" v-on:change="getSelectCondition">
                             <el-option v-for="item in options.courseName" :key="item.id" :label="item.courseName" :value="item.courseName"></el-option>
                         </el-select>
                     </el-form-item>
@@ -170,9 +168,20 @@
     `;
     //教学班筛选条件
     const conditionQuery = `
-        query($universityId: Long){
-            university(id: $universityId){
-                teacherNameList{
+        query(
+            $universityId: Long
+            $teacherName: String
+            $courseName: String
+            $schoolYear: String
+            $term: Int
+        ){
+            university(
+                id: $universityId
+            ){
+                teacherNameList(
+                    schoolYear: $schoolYear
+                    term: $term
+                    ){
                     id
                     teacherName
                 }
@@ -180,11 +189,20 @@
                     id
                     schoolYear
                 }
-                courseNameList{
+                courseNameList(
+                    schoolYear: $schoolYear
+                    term: $term
+                    teacherName: $teacherName
+                    ){
                     id
                     courseName
                 }
-                courseTimeList{
+                courseTimeList(
+                    teacherName: $teacherName
+                    courseName: $courseName
+                    schoolYear: $schoolYear
+                    term: $term
+                    ){
                     id
                     courseTime
                 }
@@ -202,10 +220,10 @@
                 loading: false,
                 showClassSelect: false,
                 options: {
-                    teacherName: null,
-                    schoolYear: null,
-                    courseName: null,
-                    courseTime: null,
+                    teacherName: [],
+                    schoolYear: [],
+                    courseName: [],
+                    courseTime: [],
                 },
                 filters: {
                     studentName: '',
@@ -219,10 +237,22 @@
                     courseTime: ''
                 },
                 physicalList: [],
+                teacherNameList: [],
             }
         },
 
         methods: {
+            querySearch(queryString, cb) {
+                var teacherNames = this.teacherNameList;
+                var results = queryString ? teacherNames.filter(this.createFilter(queryString)) : teacherNames;
+                // 调用 callback 返回建议列表的数据
+                cb(results);
+            },
+            createFilter(queryString) {
+                return (teacherName) => {
+                return (teacherName.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0);
+                };
+            },
             //开始新的筛选，同时去除班级id
             getNewPhysicalTest() {
                 let _this = this;
@@ -239,7 +269,7 @@
                     "pageSize": this.pageSize,
                     "pageNumber": this.pageNumber
                 };
-                if (typeof(this.classId) != 'undefined'  && this.classId !== -1) {
+                if (typeof(this.classId) != 'undefined' && this.classId !== -1) {
                     params.classId = this.classId
                     _this.showClassSelect = true;
                 } else {
@@ -288,24 +318,51 @@
                         this.physicalList = res.data.data.allstudents.data;
                     });
             },
-            //教学班筛选条件获取
+            //根据前置条件获取教学班筛选条件
             getSelectCondition(){
                 let _this = this;
-                _this.schoolYearList = [];
+                let params = {
+                    "universityId": this.universityId
+                };
+                if (this.filters.schoolYear !== '') {
+                    params.schoolYear = this.filters.schoolYear
+                }
+                if (this.filters.term !== '') {
+                    params.term = parseInt(this.filters.term)
+                }
+                if (this.filters.teacherName !== '') {
+                    params.teacherName = this.filters.teacherName
+                }
+                if (this.filters.courseName !== ''){
+                    params.courseName = this.filters.courseName
+                }
+                this.getCondition(params)
+            },
+            //教学班筛选条件获取
+            getCondition(params){
+                let _this = this;
+                _this.options.schoolYear = [];
+                _this.options.teacherName = [];
+                _this.options.courseName = [];
+                _this.options.courseTime = [];
                 _this.teacherNameList = [];
-                _this.courseNameList = [];
-                _this.courseTimeList = [];
                 this.$ajax.post(`${resources.graphQlApi}`, {
                                 'query': `${conditionQuery}`,
-                                variables: {
-                                    "universityId":this.universityId
-                                }       
+                                variables: params
                     })
                 .then(res => {
                      _this.options.schoolYear = res.data.data.university.schoolYearList;
+                     if (this.filters.schoolYear =='') {
+                        this.filters.schoolYear = _this.options.schoolYear[0].schoolYear;
+                     }
                      _this.options.teacherName = res.data.data.university.teacherNameList;
                      _this.options.courseName = res.data.data.university.courseNameList;
                      _this.options.courseTime = res.data.data.university.courseTimeList;
+                     for (var i = 0;i < _this.options.teacherName.length;i ++) {
+                         this.teacherNameList.push({
+                             "value": this.options.teacherName[i].teacherName
+                         })
+                     }
                 });
             }
 
